45e5ef4ac2db6f27c1e91367ac81b1fa5d2262b2
victorialilly <victorialilly06@gmail.com>
2020-09-18 14:00:28 +0000

    Built creature

--- index.java
+++ index.java
@@ -7,14 +7,21 @@ void draw() {
   ellipseMode(CENTER);
   ellipse(mouseX, mouseY, 1, 1);
   println(mouseX, mouseY);
-  rectMode(CENTER);
+  rectMode(CORNER);
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
-  
- 
-  
-  
+fill(100,23,93)
+ellipse(200,200,100,100)
+fill(255,255,255)
+ellipse(200,200,70,70)
+fill(0,0,0)
+ellipse(200,200,50,50)
+stroke(100,23,93)
+line(151,202,74,238)
+line(251,202,325,238)
+fill(100,23,93)
+rect(180,245,40,100)
  
 
   
