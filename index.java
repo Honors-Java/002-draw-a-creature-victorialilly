c2d3255d3c41f668412dd4f0013c7fae259acf09
Steven Tidd <steven.tidd@gmail.com>
2020-09-18 09:46:59 -0400

    Initial commit

---
+++ index.java
@@ -0,0 +1,24 @@
+void setup() {
+	size(400, 400);
+}
+
+void draw() {
+  background(255);
+  ellipseMode(CENTER);
+  ellipse(mouseX, mouseY, 1, 1);
+  println(mouseX, mouseY);
+  rectMode(CENTER);
+ 
+  //Make a creature!!! 
+  //all of your programming should be after this 👍
+  
+ 
+  
+  
+ 
+
+  
+ 
+	
+
+}
