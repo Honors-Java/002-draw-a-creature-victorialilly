03d91194c655e6e66bf7cd037defeb0893837829
victorialilly <victorialilly06@gmail.com>
2020-09-18 14:02:33 +0000

    added comments to code

--- index.java
+++ index.java
@@ -11,19 +11,20 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
+// eye of the creature
 fill(100,23,93)
 ellipse(200,200,100,100)
 fill(255,255,255)
 ellipse(200,200,70,70)
 fill(0,0,0)
 ellipse(200,200,50,50)
+// arms
 stroke(100,23,93)
 line(151,202,74,238)
 line(251,202,325,238)
+ //leg thing of the creature
 fill(100,23,93)
 rect(180,245,40,100)
- 
-
   
  
 	
